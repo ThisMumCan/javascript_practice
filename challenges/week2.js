90d024f2b84bf7b55e480bc4c68ad96e1680ddbd
zoe kelman <zoey247@aol.com>
2019-04-13 16:51:02 +0100

    Progressing through week2 tasks

--- challenges/week2.js
+++ challenges/week2.js
@@ -9,57 +9,53 @@ function getFillings(sandwich) {
     else if (breadType === "rye") {
       return "smoked salmon", "dill";
     }
-  }
-
-
-/*
-if bread = sourdough
-return brie relish lettuce
-
-else if bread = rye
-return smoked salmon dill
-sandwich 1 = 
-bread = sourdough
-fillings = brie relish lettuce
-accompaniment = crisps
-return = fillings
-
-sandwich 2
-bread = rye
-fillings = smoked salmon dill
-accomianment = crisps
-return fillings
-
-*/
 
 function isFromManchester(person) {
-      if (person === undefined) throw new Error("person is required");
-      // Your code here!
+  if (person === undefined) throw new Error("person is required");
+  person.forEach(function (city) {
+    const cityType = city.type;
+    if (cityType === "Manchester") {
+      return "true";
     }
-/*
-*/
+    else {
+      return "false";
+    }
+  
 function getBusNumbers(people) {
-      if (people === undefined) throw new Error("people is required");
-      // Your code here!
-    }
+  if (people === undefined) throw new Error("people is required");
+  const additionalBuses = people/40
+
+  if (people <= 40){
+    return 1;
+  }
+  else if (people > 40){
+    return 1 + math.ceil(additionalBuses)
+  }
+}
 /*
+if people =< 40 return 1 bus
+else if people => need more than one bus
+return number of buses required
+maths number of people/40 = number of buses.rounded up?
+
+
 */
 function countSheep(arr) {
-      if (!arr) throw new Error("arr is required");
-      // Your code here!
-    }
+  if (!arr) throw new Error("arr is required");
+  // Your code here!
+}
 /*
 */
 function hasMPostCode(person) {
-      if (person === undefined) throw new Error("person is required");
-      // Your code here!
-    }
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+}
 /*
 */
 module.exports = {
-      getFillings,
-      isFromManchester,
-      countSheep,
-      getBusNumbers,
-      hasMPostCode
-    };
+  getFillings,
+  isFromManchester,
+  countSheep,
+  getBusNumbers,
+  hasMPostCode
+};
